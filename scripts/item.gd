f09e1762a2584d906dc8a7250c699fa4a2c3457f
Vinícius <viniciushallmann2015@gmail.com>
2025-03-16 13:32:31 -0300

    fix size and centralize item on display

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,14 +1,13 @@
 extends TextureRect
 
-@export var custom_size = Vector2(50, 50)
-
 func set_item_image(item_name: String) -> void:
-	print(item_name)
 	texture = load("res://assets/images/%s.png" % item_name)
 	
 	var texture_size = texture.get_size()
 	scale = Vector2(
-		custom_size.x / texture_size.x,
-		custom_size.y / texture_size.y
+		Globals.item_size.x / texture_size.x,
+		Globals.item_size.y / texture_size.y
 	)
-	position = Vector2(0,0)
+	await get_tree().process_frame 
+
+	position = (get_parent().size / 2) - (Globals.item_size / 2)
